4b39c8722cc31d7589da65df4f32adbab51050df
Jim Safley <jimsafley@gmail.com>
2016-02-21 12:16:47 -0500

    Add explanation text to corpus form

--- views/admin/corpora/form.php
+++ views/admin/corpora/form.php
@@ -16,6 +16,8 @@ $textElementSetName = $textElement->getElementSet()->name;
             <label for="sequence_element_id" class="required">Sequence Element</label>
         </div>
         <div class="inputs five columns omega">
+            <p class="explanation">Select the element that contains the sequence text 
+            for every item of this corpus. Items without this element are ignored.</p>
             <?php echo $this->formSelect(
                 'sequence_element_id',
                 $corpus->sequence_element_id,
@@ -29,6 +31,8 @@ $textElementSetName = $textElement->getElementSet()->name;
             <label for="sequence_type" class="required">Sequence Type</label>
         </div>
         <div class="inputs five columns omega">
+            <p class="explanation">Select the type of sequence to validate and represent
+            on the x-axis of the ngram viewer.</p>
             <?php echo $this->formSelect(
                 'sequence_type',
                 $corpus->sequence_type,
@@ -42,6 +46,9 @@ $textElementSetName = $textElement->getElementSet()->name;
             <label for="sequence_range">Sequence Range</label>
         </div>
         <div class="inputs five columns omega">
+            <p class="explanation">Enter the range that the sequence must come between,
+            in the format <code>START-END</code>. The start and end must conform
+            to the standardized format of the selected sequence type.</p>
             <?php echo $this->formText('sequence_range', $corpus->sequence_range); ?>
         </div>
     </div>
@@ -50,6 +57,9 @@ $textElementSetName = $textElement->getElementSet()->name;
             <label for="query">Item Pool Query</label>
         </div>
         <div class="inputs five columns omega">
+            <p class="explanation">Filter the item pool (the initial, non-validated
+            pool of items) using a URL query string that comes as a result of submitting
+            an advanced item search.</p>
             <?php echo $this->formText('query', $corpus->query); ?>
         </div>
     </div>
