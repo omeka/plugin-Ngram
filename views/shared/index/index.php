d50cae27a1583f11e4d55f90031b1b77b55d1c3b
Jim Safley <jimsafley@gmail.com>
2016-02-23 15:51:13 -0500

    Prompt when there are no corpora to search

--- views/shared/index/index.php
+++ views/shared/index/index.php
@@ -7,6 +7,7 @@ if ($this->dataJson) {
 }
 echo head(array('title' => 'Ngram Viewer'));
 ?>
+<?php if ($this->corporaOptions): ?>
 <form method="post">
     Graph these comma-separated phrases: <?php echo $this->formText('queries', $this->queries, array('size' => 40, 'style' => 'margin-bottom:4px')); ?><br>
     between <?php echo $this->formText('start', $this->start, array('size' => 8, 'style' => 'margin-bottom:4px')); ?>
@@ -21,4 +22,7 @@ echo head(array('title' => 'Ngram Viewer'));
     data-graph-config="<?php echo $this->escape(json_encode($this->graphConfig)); ?>"
     data-data-keys-value="<?php echo $this->escape(json_encode($this->dataKeysValue)); ?>"
     data-data-json="<?php echo $this->escape(json_encode($this->dataJson)); ?>"></div>
+<?php else: ?>
+<p>No corpora to search.</p>
+<?php endif; ?>
 <?php echo foot(); ?>
